f37fd923bc7d0663413d483d16ff114c53a48d0d
rishabaggarwal247 <rishabaggarwal247@gmail.com>
2019-12-07 20:06:24 +0530

    login & register screen skeleton added

---
+++ app/src/main/java/com/cu/project/Welcome.java
@@ -0,0 +1,35 @@
+package com.cu.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcome extends AppCompatActivity {
+Button signinwelcomebtn ,registerwelcomebtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        signinwelcomebtn = findViewById(R.id.signinwelcomebutton);
+        registerwelcomebtn = findViewById(R.id.registerwelcomebutton);
+        signinwelcomebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Welcome.this, Login.class);
+                startActivity(myIntent);
+            }
+        });
+        registerwelcomebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Welcome.this, Register.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+}
