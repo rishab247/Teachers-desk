69d1ee39fd347be123d99e798b978daf2731318b
Tushar Tambi <44211831+8426988382@users.noreply.github.com>
2019-12-08 17:59:19 +0530

    Profile page and View Page with Top navigation bar are added.

--- app/src/main/java/com/cu/project/View_fragment.java
+++ app/src/main/java/com/cu/project/View_fragment.java
@@ -1,19 +1,25 @@
 package com.cu.project;
 
-import android.content.Context;
-import android.net.Uri;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.sql.Time;
 
 
 public class View_fragment extends Fragment {
+    Button upload_btn;
 
 
     @Override
@@ -21,6 +27,23 @@ public class View_fragment extends Fragment {
     {
         setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_view_fragment,container,false);
+
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        upload_btn =  getView().findViewById(R.id.uploadbtn_ID);
+
+        upload_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), Upload.class);
+                startActivity(intent);
+                Toast.makeText(getContext() , "Button Clicked" , Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
