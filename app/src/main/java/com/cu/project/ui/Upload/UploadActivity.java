68a5b0901b8ea670392da4babb241e2954c7461f
Tushar Tambi <44211831+8426988382@users.noreply.github.com>
2019-12-27 12:40:45 +0530

    Upload page design added.

--- app/src/main/java/com/cu/project/ui/Upload/UploadActivity.java
+++ app/src/main/java/com/cu/project/ui/Upload/UploadActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 
 import android.os.Bundle;
@@ -11,18 +13,27 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.cu.project.R;
 import com.cu.project.ui.Profiile.ProfileActivity;
 
+import java.util.Calendar;
 import java.util.zip.Inflater;
 
 public class UploadActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     Button savebtn;
+    Button btndatepicker;
+    private int mYear, mMonth, mDay;
+    Spinner spinner , spinner1;
+    EditText txtDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +45,23 @@ public class UploadActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(UploadActivity.this , ProfileActivity.class);
+                Intent intent = new Intent(UploadActivity.this, ProfileActivity.class);
                 startActivity(intent);
             }
         });
+
+        spinner = findViewById(R.id.Catagory);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.country_arrays, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner1 = findViewById(R.id.type_id);
+        spinner1.setAdapter(adapter);
+
+
+        btndatepicker = findViewById(R.id.btn_date);
+        txtDate = findViewById(R.id.in_date);
+
 
     }
 
@@ -54,4 +78,6 @@ public class UploadActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext() , "Saved", Toast.LENGTH_LONG).show();
         return true;
     }
+
+
 }
