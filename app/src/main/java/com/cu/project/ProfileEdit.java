3c7c84f2dac0d955835076ec0a70ec35a49c8102
Tushar Tambi <44211831+8426988382@users.noreply.github.com>
2020-02-12 19:30:34 +0530

    Download the view and upload a profile pic.

--- app/src/main/java/com/cu/project/ProfileEdit.java
+++ app/src/main/java/com/cu/project/ProfileEdit.java
@@ -2,12 +2,18 @@ package com.cu.project;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,22 +25,27 @@ import com.cu.project.APIHelper.ApiEditProfile;
 import com.cu.project.APIHelper.ApiPassword;
 import com.cu.project.ui.detailclass;
 import com.cu.project.ui.login.loginActivity;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.util.concurrent.ExecutionException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileEdit extends AppCompatActivity {
 
-    TextView chagepass , saveUserDataButton , cancelUserDataButton;
+    TextView chagepass, saveUserDataButton, cancelUserDataButton;
     private View popupInputDialog;
     private View popupInputDialogprofile;
     private EditText oldpasswordEditText, newpasswordEditText;
 
 
-    EditText fname , dob , doj , email , eid , pno , quali , depart, uni;
+    EditText fname, dob, doj, email, eid, pno, quali, depart, uni;
 
     Button savebtn;
     private EditText confirmpass;
-    private TextView canclepass , savepass;
+    private TextView canclepass, savepass;
 
     String password;
     String p_no;
@@ -45,6 +56,10 @@ public class ProfileEdit extends AppCompatActivity {
 
     String university;
 
+    CircleImageView img_pro;
+
+    int RESULT_LOAD_IMAGE = 1;
+    Uri imageuri;
 
 
     @Override
@@ -88,8 +103,7 @@ public class ProfileEdit extends AppCompatActivity {
                             flag = 1;
                         }
 
-                        if(flag == 0)
-                        {
+                        if (flag == 0) {
                             oldpass = loginActivity.generatedhash12(oldpass);
                             oldpass = loginActivity.generatedhash12(oldpass);
 
@@ -229,18 +243,36 @@ public class ProfileEdit extends AppCompatActivity {
 
             }
         });
+
+
+        img_pro = findViewById(R.id.profile_img);
+
+        img_pro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Intent gallery = new Intent(Intent.ACTION_PICK);
+//                gallery.setType("Image/*");
+//                startActivityForResult(Intent.createChooser(gallery, "Select Picture"), RESULT_LOAD_IMAGE);
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(Intent.createChooser(i, "Select Picture"), RESULT_LOAD_IMAGE);
+
+            }
+        });
     }
 
-    private void initPopupViewControls()
-    {
+    private void initPopupViewControls() {
         // Get layout inflater object.
         LayoutInflater layoutInflater = LayoutInflater.from(ProfileEdit.this);
 
         // Inflate the popup dialog from a layout xml file.
         popupInputDialog = layoutInflater.inflate(R.layout.passchangedialog, null);
 
-        oldpasswordEditText =  popupInputDialog.findViewById(R.id.oldpass);
-        newpasswordEditText =  popupInputDialog.findViewById(R.id.newpass);
+        oldpasswordEditText = popupInputDialog.findViewById(R.id.oldpass);
+        newpasswordEditText = popupInputDialog.findViewById(R.id.newpass);
         saveUserDataButton = popupInputDialog.findViewById(R.id.buttonsaveuserdata);
         cancelUserDataButton = popupInputDialog.findViewById(R.id.buttoncanceluserdata);
 
@@ -248,14 +280,13 @@ public class ProfileEdit extends AppCompatActivity {
     }
 
 
-    private void initPopupViewControlsprofileedit()
-    {
+    private void initPopupViewControlsprofileedit() {
         // Get layout inflater object.
         LayoutInflater layoutInflater = LayoutInflater.from(ProfileEdit.this);
 
         popupInputDialogprofile = layoutInflater.inflate(R.layout.passdialog, null);
 
-        confirmpass =  popupInputDialogprofile.findViewById(R.id.confirmpass);
+        confirmpass = popupInputDialogprofile.findViewById(R.id.confirmpass);
 
         savepass = popupInputDialogprofile.findViewById(R.id.btnconfirm);
         canclepass = popupInputDialogprofile.findViewById(R.id.canclepass);
@@ -263,4 +294,65 @@ public class ProfileEdit extends AppCompatActivity {
 
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RESULT_LOAD_IMAGE  && resultCode == RESULT_OK) {
+            imageuri = data.getData();
+
+
+            CropImage.activity(imageuri)
+                    .setAspectRatio(1 , 1)
+                    .start(this);
+
+        }
+
+            if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
+            {
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+                if(resultCode == RESULT_OK)
+                {
+                    Uri resulturi = result.getUri();
+                    try {
+
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver() , resulturi);
+
+
+
+                        Bitmap converetdImage = getResizedBitmap(bitmap, 200);
+                        img_pro.setImageBitmap(converetdImage);
+
+                        int ans = converetdImage.getByteCount();
+
+                        Log.e("SIZE" , String.valueOf(ans));
+
+
+
+                    }catch (Exception e)
+                    {
+                        Log.e("EXCEPTION IMAGE" , e.getMessage());
+                    }
+                }
+
+            }
+
+    }
+
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float)width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
 }
--- app/src/main/java/com/cu/project/ui/detailclass.java
+++ app/src/main/java/com/cu/project/ui/detailclass.java
@@ -1,16 +1,36 @@
 package com.cu.project.ui;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.SystemClock;
 import android.provider.DocumentsContract;
+import android.text.Layout;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,49 +38,77 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 
 import com.cu.project.APIHelper.ApiDelete;
 import com.cu.project.APIHelper.ApigetPaper;
 import com.cu.project.APIHelper.Apigetdetails;
 import com.cu.project.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
-import org.w3c.dom.Document;
-
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
+
+import static com.cu.project.R.layout.accomplishments_details;
 
 public class detailclass  extends AppCompatActivity {
 
 
-    FloatingActionButton downloadfbtn ,deletebtn;
-    TextView Title , Date , URL , publisher , appnumber , authname;
-    TextView tohideurl , tohidepublisher , tohideapp , tohideauthor, typetext;
+    private static final String TAG = "PdfCreatorActivity";
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 111;
+    private File pdfFile;
+
+    private View mRootView;
+    Context context;
+
+    FloatingActionButton downloadfbtn, deletebtn;
+    TextView Title, Date, URL, publisher, appnumber, authname;
+    TextView tohideurl, tohidepublisher, tohideapp, tohideauthor, typetext;
     TextView des;
 
     private View popupInputDialogView = null;
     EditText passwordEditText;
     private TextView cancelUserDataButton;
     private TextView saveUserDataButton;
-    String id = null , type = null;
+    String id = null, type = null;
 
     private ArrayList<String> names = new ArrayList<>();
     private ArrayList<String> emails = new ArrayList<>();
     private ArrayList<String> pnos = new ArrayList<>();
     private ArrayList<String> details = new ArrayList<>();
+    private static final int STORAGE_CODE = 1000;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.accomplishments_details);
+        setContentView(accomplishments_details);
 
+        context = this;
         Bundle bundle = getIntent().getExtras();
 
-        HashMap<String , ArrayList> info = (HashMap<String, ArrayList>) bundle.get("info");
+        HashMap<String, ArrayList> info = (HashMap<String, ArrayList>) bundle.get("info");
 
 
         tohideurl = findViewById(R.id.textView10);
@@ -85,11 +133,9 @@ public class detailclass  extends AppCompatActivity {
 
         String authdetails = "";
 
-        for(int i =0; i < names.size() ; i ++)
-        {
+        for (int i = 0; i < names.size(); i++) {
             authdetails += names.get(i) + "\n";
         }
-
 
 
         int last = details.size();
@@ -102,7 +148,7 @@ public class detailclass  extends AppCompatActivity {
         Log.e("TYPE ", type);
 
 
-        if(type.equals("Honors_and_Award")){
+        if (type.equals("Honors_and_Award")) {
 
             Title.setText(details.get(0));
             tohideurl.setText("Issuer");
@@ -128,8 +174,7 @@ public class detailclass  extends AppCompatActivity {
             appnumber.setVisibility(View.GONE);
 
 
-        }
-        else if(type.equals("Publication")){
+        } else if (type.equals("Publication")) {
 
             Title.setText(details.get(0));
             publisher.setText(details.get(1));
@@ -155,9 +200,7 @@ public class detailclass  extends AppCompatActivity {
 
             authname.setText(authdetails);
 
-        }
-        else if(type.equals("Patent"))
-        {
+        } else if (type.equals("Patent")) {
 
             Title.setText(details.get(0));
             tohidepublisher.setText("Patent Office");
@@ -179,10 +222,7 @@ public class detailclass  extends AppCompatActivity {
             tohideauthor.setText("Inventors");
 
 
-
-
-        }
-        else{
+        } else {
             Title.setText(details.get(0));
 
 
@@ -208,16 +248,6 @@ public class detailclass  extends AppCompatActivity {
 
 
         }
-
-
-
-
-
-
-
-
-
-        downloadfbtn = findViewById(R.id.floatingActionButton1);
         deletebtn = findViewById(R.id.deletepaperid);
 
         deletebtn.setOnClickListener(new View.OnClickListener() {
@@ -235,27 +265,22 @@ public class detailclass  extends AppCompatActivity {
                 alertDialog.show();
 
 
-
                 saveUserDataButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(getApplicationContext() , "Your Data will be removed permanently" , Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Your Data will be removed permanently", Toast.LENGTH_SHORT).show();
 
-                        if(passwordEditText.getText().toString().trim().equals(""))
-                        {
+                        if (passwordEditText.getText().toString().trim().equals("")) {
                             passwordEditText.setError("Field cannot be empty");
-                        }
-                        else
-                        {
-                            String pass =  passwordEditText.getText().toString().trim();
+                        } else {
+                            String pass = passwordEditText.getText().toString().trim();
                             alertDialog.cancel();
-                            if(type.equals("Honors_and_Award"))
+                            if (type.equals("Honors_and_Award"))
                                 type = "HonorsandAward";
 
-                            ApiDelete apiDelete = new ApiDelete(detailclass.this , id, type , pass);
+                            ApiDelete apiDelete = new ApiDelete(detailclass.this, id, type, pass);
                             apiDelete.execute();
                         }
-
 
 
                     }
@@ -270,20 +295,135 @@ public class detailclass  extends AppCompatActivity {
             }
         });
 
+
+
+        downloadfbtn = findViewById(R.id.floatingActionButton1);
+
+        downloadfbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+
+                        String[] permission = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permission , STORAGE_CODE);
+                    }
+                    else{
+                        savePdf();
+                    }
+                }
+                else{
+                    savePdf();
+                }
+            }
+        });
     }
 
-    private void initPopupViewControls()
-    {
+    private void savePdf() {
+
+        Document mDoc = new Document();
+
+        String mFileName = new SimpleDateFormat("yyyyMMdd_HHmmss" , Locale.getDefault()).format(System.currentTimeMillis());
+
+        String mFilePath = Environment.getExternalStorageDirectory() + "/" + mFileName + ".pdf";
+
+        try {
+
+
+            Bitmap screen = getBitmapFromView(this.getWindow().findViewById(R.id.layout));
+
+            PdfWriter.getInstance(mDoc , new FileOutputStream(mFilePath));
+
+            mDoc.open();
+
+            LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+            ConstraintLayout root = (ConstraintLayout) inflater.inflate(accomplishments_details, null);
+            root.setDrawingCacheEnabled(true);
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            screen.compress(Bitmap.CompressFormat.PNG, 50, stream);
+            byte[] byteArray = stream.toByteArray();
+
+            Image image = null;
+
+            image = Image.getInstance(byteArray);
+
+            mDoc.add(image);
+
+            Log.e("PRINT FILE NAME" , mFileName);
+
+
+            mDoc.close();
+
+            Toast.makeText(this , "File Saved" , Toast.LENGTH_SHORT).show();
+
+        }catch (Exception e)
+        {
+            Toast.makeText(this  ,   e.getMessage() , Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+            case STORAGE_CODE:{
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    savePdf();
+                }
+                else{
+                    Toast.makeText(this , "Permission Denied" , Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
+    }
+
+    private void initPopupViewControls() {
         // Get layout inflater object.
         LayoutInflater layoutInflater = LayoutInflater.from(detailclass.this);
 
         // Inflate the popup dialog from a layout xml file.
         popupInputDialogView = layoutInflater.inflate(R.layout.deletepopup, null);
 
-        passwordEditText =  popupInputDialogView.findViewById(R.id.pass1);
+        passwordEditText = popupInputDialogView.findViewById(R.id.pass1);
         saveUserDataButton = popupInputDialogView.findViewById(R.id.button_save_user_data);
         cancelUserDataButton = popupInputDialogView.findViewById(R.id.button_cancel_user_data);
 
 
     }
+
+
+    public static Bitmap getBitmapFromView(View v) {
+        v.clearFocus();
+        v.setPressed(false);
+
+        boolean willNotCache = v.willNotCacheDrawing();
+        v.setWillNotCacheDrawing(false);
+
+        int color = v.getDrawingCacheBackgroundColor();
+        v.setDrawingCacheBackgroundColor(0);
+
+        if (color != 0) {
+            v.destroyDrawingCache();
+        }
+        v.buildDrawingCache();
+        Bitmap cacheBitmap = v.getDrawingCache();
+        if (cacheBitmap == null) {
+            return null;
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(cacheBitmap);
+
+        v.destroyDrawingCache();
+        v.setWillNotCacheDrawing(willNotCache);
+        v.setDrawingCacheBackgroundColor(color);
+
+        return bitmap;
+    }
+
+
 }
